51e67edbcfa404b18462264f955526d9b34053b9
chandangupta1997 <chandanguptajobs@gmail.com>
2022-08-06 13:24:14 +0530

    starter

--- src/Route/router.js
+++ src/Route/router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userModel = require('../Models/userModel')
+const userController = require('../controller/userController')
 
 const route = express.Router()
 
@@ -10,36 +11,38 @@ route.get('/testMe', function(req,res){
 
 
 
-route.post('/userDetails',async function(req,res){
+// route.post('/userDetails',async function(req,res){
 
 
-    let data = req.body 
+//     let data = req.body 
 
-    let {nickName,questions} = data
-    // for validation and all 
+//     let {nickName,questions} = data
+//     // for validation and all 
    
 
-    //questions =JSON.parse(questions)
+//     //questions =JSON.parse(questions)
 
-    let DataForDb ={
-        nickName:nickName,
-        questions:{
-            struggleTime:questions.struggleTime,
-            bedTime:questions.bedTime,
-            wakeUpTime:questions.wakeUpTime,
-            sleepHours:questions.sleepHours
-        }
+//     let DataForDb ={
+//         nickName:nickName,
+//         questions:{
+//             struggleTime:questions.struggleTime,
+//             bedTime:questions.bedTime,
+//             wakeUpTime:questions.wakeUpTime,
+//             sleepHours:questions.sleepHours
+//         }
 
-    }
+//     }
 
-    let createdData = await userModel.create(DataForDb)
+//     let createdData = await userModel.create(DataForDb)
 
-    res.send(createdData)
+//     res.send(createdData)
 
 
 
-})
+// })
 
+
+route.post('/nickName')
 
 
 
